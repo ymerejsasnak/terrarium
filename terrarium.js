dd3db52ea3c0791b063932c52a9204d058925b7b
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-12-05 11:08:25 -0500

    created LifelikeWorld with Plants and PlantEaters

--- terrarium.js
+++ terrarium.js
@@ -85,6 +85,65 @@ World.prototype.toString = function() {   //this would be the main one to refact
 }
 
 
+function LifelikeWorld(map, legend) {
+  World.call(this, map, legend);
+}
+LifelikeWorld.prototype = Object.create(World.prototype);
+
+LifelikeWorld.prototype.letAct = function(critter, vector) {
+  var action = critter.act(new View(this, vector));
+  var handled = action && action.type in actionTypes && 
+                actionTypes[action.type].call(this, critter, vector, action);
+  if (!handled) {
+    critter.energy -= 0.2;
+    if (critter.energy <= 0) {
+      this.grid.set(vector, null);
+    }
+  }
+}
+
+
+var actionTypes = Object.create(null);
+
+actionTypes.grow = function(critter) {
+  critter.energy += 0.5;
+  return true;
+}
+
+actionTypes.move = function(critter, vector, action) {
+  var dest = this.checkDestination(action, vector);
+  if (dest === null || critter.energy <= 1 || this.grid.get(dest) != null) {
+    return false;
+  }
+  critter.energy -= 1;
+  this.grid.set(vector, null);
+  this.grid.set(dest, critter);
+  return true;
+}
+
+actionTypes.eat = function(critter, vector, action) {
+  var dest = this.checkDestination(action, vector);
+  var atDest = dest != null && this.grid.get(dest);
+  if (!atDest || atDest.energy === null) {
+    return false;
+  }
+  critter.energy += atDest.energy;
+  this.grid.set(dest, null);
+  return true;
+}
+
+actionTypes.reproduce = function(critter, vector, action) {
+  var baby = elementFromChar(this.legend, critter.originChar);
+  var dest = this.checkDestination(action, vector);
+  if (dest === null || critter.energy <= 2 * baby.energy || this.grid.get(dest) != null) {
+    return false;
+  }
+  critter.energy -= 2 * baby.energy;
+  this.grid.set(dest, baby);
+  return true;
+}
+
+
 function elementFromChar(legend, ch) {
   if (ch === " ") {
     return null;
@@ -157,6 +216,9 @@ function dirPlus(dir, n) { //direction is from above list, n is number of 45degr
 }
 
 
+//=====================================================================
+//THESE CRITTERS WORK WITH ORIGINAL WORLD OBJECT, NOT IN NEW LIFELIKE WORLD OBJECT
+
 function WallFollower() {
   this.direction = "s";
 }
@@ -186,28 +248,69 @@ BouncingCritter.prototype.act = function(view) {
 }
 
 
-function Wall() {} //does nothing at all  
+//===================================================
+
+
+//LIFELIKEWORLD CRITTERS
+
+function Wall() {} //does nothing at all  (works in either world object)
+
+
+function Plant() {
+  this.energy = 3 + Math.random() * 4;
+}
+Plant.prototype.act = function(context) {
+  if (this.energy > 15) {
+    var space = context.find(" ");
+    if (space) {
+      return {type: "reproduce", direction: space};
+    }
+  }
+  if (this.energy < 20) {
+    return {type: "grow"};
+  }
+}
+
+
+function PlantEater() {
+  this.energy = 20;
+}
+PlantEater.prototype.act = function(context) {
+  var space = context.find(" ");
+  if (this.energy > 60 && space) {
+    return {type: "reproduce", direction: space};
+  }
+  var plant = context.find("*");  //plant symbol...put these into an object or something later
+  if (plant) {
+    return {type: "eat", direction: plant};
+  }
+  if (space) {
+    return {type: "move", direction: space};
+  }
+}
 
 
 
 
 
+var valley = ["############################",
+              "#####                 ######",
+              "##   ***                **##",
+              "#   *##**         **  O  *##",
+              "#    ***     O    ##**    *#",
+              "#       O         ##***    #",
+              "#                 ##**     #",
+              "#   O       #*             #",
+              "#*          #**       O    #",
+              "#***        ##**    O    **#",
+              "##****     ###***       *###",
+              "############################"];
 
+var legend = {"#": Wall,
+              "O": PlantEater,
+              "*": Plant};
 
-var plan = ["############################",
-            "#      #    #             ##",
-            "#                          #",
-            "#          #####           #",
-            "##         #   #    ##     #",
-            "###           ##     #     #",
-            "#           ###      #     #",
-            "#   ####                   #",
-            "#   ##            O        #",
-            "#    #      }          ### #",
-            "#    #                     #",
-            "############################"];
-
-var world = new World(plan, {"#": Wall, "}": WallFollower, "O": BouncingCritter});
+var world = new LifelikeWorld(valley, legend);
 
 $(document).ready(function() {
 
